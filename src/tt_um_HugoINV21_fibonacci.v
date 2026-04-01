4f2cb4d54741ffe12c20e5bb22694db2dfe3fc45
HugoINV21 <hugo.nunez.v@mail.pucv.cl>
2026-04-01 17:41:17 -0300

    Mi generador de Fibonacci para Tiny Tapeout

--- src/tt_um_HugoINV21_fibonacci.v
+++ src/tt_um_HugoINV21_fibonacci.v
@@ -1,22 +1,39 @@
+`default_nettype none
 
 module tt_um_HugoINV21_fibonacci (
-    input clk,
-    output reg [7:0] fib
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-reg [7:0] a = 0, b = 1;
+reg [7:0] a;
+reg [7:0] b;
+reg [7:0] fib;
 
-always @(posedge clk) begin
-    a <= b;
-    b <= a + b;
-    fib <= a;
+assign uo_out = fib;
+
+// No usamos IOs bidireccionales
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        a   <= 8'd0;
+        b   <= 8'd1;
+        fib <= 8'd0;
+    end else begin
+        a   <= b;
+        b   <= a + b;
+        fib <= a;
+    end
 end
 
 endmodule
-
-/* 
- * Do not change Module name 
-*/
 
 
 
